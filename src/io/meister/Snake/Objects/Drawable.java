743e4ec6c30191f47a5319de73bd42bd80e22794
manuelmeister <news.manuelsworld@gmail.com>
2015-06-15 14:41:13 +0200

    PHPDocs

--- src/io/meister/Snake/Objects/Drawable.java
+++ src/io/meister/Snake/Objects/Drawable.java
@@ -3,7 +3,7 @@ package io.meister.Snake.Objects;
 import java.awt.*;
 
 /**
- * Created by manuelmeister on 11.05.15.
+ * Item that can be drawed via Graphics
  */
 public interface Drawable {
     void draw(Graphics g);
