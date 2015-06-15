5d6b3d4140a92abded70c08efced7fe54ff56926
manuelmeister <news.manuelsworld@gmail.com>
2015-06-15 14:42:34 +0200

    Added customizations

--- src/io/meister/Snake/View/GUI.java
+++ src/io/meister/Snake/View/GUI.java
@@ -1,26 +1,22 @@
 package io.meister.Snake.View;
 
 import io.meister.Snake.Controller.Game;
-import io.meister.Snake.Controller.Vector2;
-import io.meister.Snake.Objects.Snake;
 
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
 
 /**
  * Die Klasse GUI stellt das Fenster (JFrame) und
- * das Spielfeld (JPanel) des Snake-Spiels zur Verfügung.
+ * das Spielfeld (JPanel) des Snake-Spiels zur Verfuegung.
  *
  * @author A. Scheidegger, M.Frieden
  * @version 4.0
  */
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame implements KeyListener {
+public class GUI extends JFrame{
 
     /**
      * Referenz auf das io.meister.Snake.Controller.Game-Objekt.
@@ -45,22 +41,21 @@ public class GUI extends JFrame implements KeyListener {
     /**
      * Konstruktor. Initialisiert den Frame. Registriert das io.meister.Snake.Controller.Game-Objekt
      * als KeyListener.
+     * @param game Game
      */
     public GUI(Game game) {
         this.game = game;
-        //TODO 12 is Bar on top
-        setSize(Game.GAMEFIELD_SIZE_X * Game.UNIT, 12 + Game.GAMEFIELD_SIZE_Y * Game.UNIT);
+        //TODO 22 is Bar on top
+        setSize(Game.GAMEFIELD_SIZE_X * Game.UNIT, 22 + Game.GAMEFIELD_SIZE_Y * Game.UNIT);
         setTitle("Snake V4.0");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setContentPane(spielfeld);
         setVisible(true);
-        addKeyListener(this);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.loop();
     }
 
     /**
-     * Gibt die Breite des Spielfeldes zurück.
+     * Gibt die Breite des Spielfeldes zurueck.
      *
      * @return Breite des Spielfeldes.
      */
@@ -69,55 +64,12 @@ public class GUI extends JFrame implements KeyListener {
     }
 
     /**
-     * Gibt die Höhe des Spielfeldes zurück.
+     * Gibt die Hoehe des Spielfeldes zurueck.
      *
-     * @return Höhe des Spielfeldes
+     * @return Hoehe des Spielfeldes
      */
     public int getHoehe() {
         return spielfeld.getHeight();
     }
 
-
-    public void loop() {
-        while (this.game.snake.state == Snake.ALIVE) {
-            try {
-                this.game.snake.move();
-                repaint();
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-                this.game.snake.dir = new Vector2(0,-1);
-                break;
-            case KeyEvent.VK_DOWN:
-                this.game.snake.dir = new Vector2(0,1);
-                break;
-            case KeyEvent.VK_LEFT:
-                this.game.snake.dir = new Vector2(-1,0);
-                break;
-            case KeyEvent.VK_RIGHT:
-                this.game.snake.dir = new Vector2(1,0);
-                break;
-            default:
-                System.out.println("Not accepted");
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
